9db9160a8294466249fd30e12df6f57f44d7ea5f
tsakuse <tsakuse@gmail.com>
2026-05-15 14:13:12 +0800

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import '../../styles/main.css'
-import { useState, useEffect } from 'react';
 
 const root = createRoot(document.getElementById('root'))
 root.render(
